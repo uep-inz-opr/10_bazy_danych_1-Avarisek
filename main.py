871c6b23da06e749e1fac7266a92175918e9857f
Szymon Roszak <szymonroszak@icloud.com>
2022-06-05 17:09:00 +0200

    Poprawka literowki

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     cur=sqlite_con.cursor()
     cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, to_subscriber data_type INTEGER, datetime data_type timestamp, duration data_type INTEGER, celltower data_type INTEGER);''') 
     with open(file, 'r') as fin:
-        reader=csv.reader(fin, delimeter=";")
+        reader=csv.reader(fin, delimiter=";")
         next(reader, None)
         rows=[x for x in reader]
         cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration, celltower) VALUES (?,?,?,?,?);", rows)
